43c14dee149efe0edcd27a5b9a9efb436982cbfd
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-10-30 18:14:36 +0700

    ditto

--- text-editor/ui/expand/expand.js
+++ text-editor/ui/expand/expand.js
@@ -15,14 +15,16 @@
             head = doc.head,
             body = doc.body,
             ui = $.ui,
+            i18n = $.config.languages.tools,
             s = $.config.classes[""],
             prefix = '.' + s,
             base = prefix + '-is-expanded',
-            style = doc.createElement('style');
+            style = doc.createElement('style'),
+            ss = base.slice(1), is_max;
         prefix = base + ' ' + prefix;
         style.textContent = '' + prefix + '-description .left,' + prefix + '-footer+.' + s + '-resize{display:none}' + base + ',' + base + ' body{position:static;overflow:hidden}' + prefix + '{border-width:0;position:fixed;top:0;right:0;bottom:0;left:0;z-index:9998;width:100%;height:100%}' + prefix + '-body,' + prefix + '-content{border-width:0;width:100%;height:100%;position:absolute;top:0;right:0;bottom:0;left:0}' + prefix + '-content{font-size:1.5em;padding:2.5em 1em 1em}' + prefix + '-footer,' + prefix + '-header{position:absolute;top:0;left:0;margin:.5em;z-index:9999;background:inherit;border:1px solid;border-color:inherit;box-shadow:0 0 2px rgba(0,0,0,.2)}' + prefix + '-footer{position:absolute;top:auto;bottom:0;left:0}' + prefix + '-description{padding:.4em .8em}' + prefix + '-description .right{padding-right:0}';
         $.config.css += '</style><style>body{padding-top:2.5em}';
-        $._.extend($.config.languages.tools, {
+        $._.extend(i18n, {
             maximize: ['Maximize', 'F11'],
             minimize: ['Minimize', 'F11']
         });
@@ -31,29 +33,30 @@
             once = 0;
         }
         function do_max() {
-            html.classList.add(base.slice(1));
-            ui.tool('maximize', false).select();
-            return do_min_tool(), false;
+            is_max = 1;
+            html.classList.add(ss);
+            i18n.expand = i18n.minimize;
+            ui.tool('expand', {
+                i: 'compress',
+                click: do_min
+            }).select();
+            return false;
         }
-        function do_min() {
-            html.classList.remove(base.slice(1));
-            ui.tool('minimize', false).select();
-            return do_max_tool(), false;
-        }
-        function do_max_tool() {
-            ui.key('f11', 'maximize');
-            ui.tool('maximize', {
+        function do_min(e) {
+            is_max = 0;
+            html.classList.remove(ss);
+            i18n.expand = i18n.maximize;
+            ui.tool('expand', {
                 i: 'expand',
                 click: do_max
             });
+            if (e) $.select();
+            return false;
         }
-        function do_min_tool() {
-            ui.key('f11', 'minimize');
-            ui.tool('minimize', {
-                i: 'compress',
-                click: do_min
-            });
-        }
-        do_max_tool();
+        do_min();
+        // Press `F11` to maximize/minimize
+        ui.key('f11', function(e, $) {
+            return is_max ? do_min(e, $) : do_max(e, $), false;
+        });
     });
 })();
