eb3ebd4adaa85f206fe43005a5346bb2342b8eeb
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-10-30 22:51:17 +0700

    Fix Expanding after Resize Event in Text Area

--- text-editor/ui/expand/expand.js
+++ text-editor/ui/expand/expand.js
@@ -1,6 +1,6 @@
 /*!
  * ==========================================================
- *  EXPAND PLUGIN FOR USER INTERFACE MODULE 1.0.1
+ *  EXPAND PLUGIN FOR USER INTERFACE MODULE 1.0.2
  * ==========================================================
  * Author: Taufik Nurrohman <https://github.com/tovic>
  * License: MIT
@@ -18,11 +18,12 @@
             i18n = $.config.languages.tools,
             s = $.config.classes[""],
             prefix = '.' + s,
-            base = prefix + '-is-expanded',
+            prefix_base = prefix + '-is-expanded',
             style = doc.createElement('style'),
-            ss = base.slice(1), is_max;
-        prefix = base + ' ' + prefix;
-        style.textContent = '' + prefix + '-description .left,' + prefix + '-footer+.' + s + '-resize{display:none}' + base + ',' + base + ' body{position:static;overflow:hidden}' + prefix + '{border-width:0;position:fixed;top:0;right:0;bottom:0;left:0;z-index:9998;width:100%;height:100%}' + prefix + '-body,' + prefix + '-content{border-width:0;width:100%;height:100%;position:absolute;top:0;right:0;bottom:0;left:0}' + prefix + '-content{font-size:1.5em;padding:2.5em 1em 1em}' + prefix + '-footer,' + prefix + '-header{position:absolute;top:0;left:0;margin:.5em;z-index:9999;background:inherit;border:1px solid;border-color:inherit;box-shadow:0 0 2px rgba(0,0,0,.2)}' + prefix + '-footer{position:absolute;top:auto;bottom:0;left:0}' + prefix + '-description{padding:.4em .8em}' + prefix + '-description .right{padding-right:0}';
+            ss = prefix_base.slice(1),
+            $$ = $.target.style, is_max;
+        prefix = prefix_base + ' ' + prefix;
+        style.textContent = '' + prefix + '-description .left,' + prefix + '-footer+.' + s + '-resize{display:none}' + prefix_base + ',' + prefix_base + ' body{position:static;overflow:hidden}' + prefix + '{border-width:0;position:fixed;top:0;right:0;bottom:0;left:0;z-index:9998;width:100%;height:100%}' + prefix + '-body,' + prefix + '-content{border-width:0;width:100%;height:100%;position:absolute;top:0;right:0;bottom:0;left:0}' + prefix + '-content{font-size:1.5em;padding:2.5em 1em 1em}' + prefix + '-footer,' + prefix + '-header{position:absolute;top:0;left:0;margin:.5em;z-index:9999;background:inherit;border:1px solid;border-color:inherit;box-shadow:0 0 2px rgba(0,0,0,.2)}' + prefix + '-footer{position:absolute;top:auto;bottom:0;left:0}' + prefix + '-description{padding:.4em .8em}' + prefix + '-description .right{padding-right:0}';
         $.config.css += '</style><style>body{padding-top:2.5em}';
         $._.extend(i18n, {
             maximize: ['Maximize', 'F11'],
@@ -40,6 +41,7 @@
                 i: 'compress',
                 click: do_min
             }).select();
+            $$.height = $$.width = $$.minHeight = $$.minWidth = "";
             return false;
         }
         function do_min(e) {
