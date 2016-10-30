5d9bdca5a9b289a6e7284001312de166822447bf
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-10-30 17:52:38 +0700

    Yo!

--- text-editor/ui/expand/expand.js
+++ text-editor/ui/expand/expand.js
@@ -1,6 +1,6 @@
 /*!
  * ==========================================================
- *  EXPAND PLUGIN FOR USER INTERFACE MODULE 1.0.0
+ *  EXPAND PLUGIN FOR USER INTERFACE MODULE 1.0.1
  * ==========================================================
  * Author: Taufik Nurrohman <https://github.com/tovic>
  * License: MIT
@@ -20,7 +20,7 @@
             base = prefix + '-is-expanded',
             style = doc.createElement('style');
         prefix = base + ' ' + prefix;
-        style.textContent = '' + prefix + '-description .left,' + prefix + '-footer+.' + s + '-resize{display:none}' + base + ',' + base + ' body{position:static;overflow:hidden}' + prefix + '{border-width:0;position:fixed;top:0;right:0;bottom:0;left:0;z-index:9998;width:100%;height:100%}' + prefix + '-body,' + prefix + '-content{border-width:0;width:100%;height:100%;position:absolute;top:0;right:0;bottom:0;left:0}' + prefix + '-content{font-size:1.5em;padding:2.5em 1em 1em}' + prefix + '-footer,' + prefix + '-header{position:absolute;top:.5em;left:.5em;z-index:9999;background:inherit;border:1px solid;border-color:inherit;box-shadow:0 0 2px rgba(0,0,0,.2)}' + prefix + '-footer{position:absolute;top:auto;bottom:.5em;left:.5em}' + prefix + '-description{padding:.4em .8em}' + prefix + '-description .right{padding-right:0}';
+        style.textContent = '' + prefix + '-description .left,' + prefix + '-footer+.' + s + '-resize{display:none}' + base + ',' + base + ' body{position:static;overflow:hidden}' + prefix + '{border-width:0;position:fixed;top:0;right:0;bottom:0;left:0;z-index:9998;width:100%;height:100%}' + prefix + '-body,' + prefix + '-content{border-width:0;width:100%;height:100%;position:absolute;top:0;right:0;bottom:0;left:0}' + prefix + '-content{font-size:1.5em;padding:2.5em 1em 1em}' + prefix + '-footer,' + prefix + '-header{position:absolute;top:0;left:0;margin:.5em;z-index:9999;background:inherit;border:1px solid;border-color:inherit;box-shadow:0 0 2px rgba(0,0,0,.2)}' + prefix + '-footer{position:absolute;top:auto;bottom:0;left:0}' + prefix + '-description{padding:.4em .8em}' + prefix + '-description .right{padding-right:0}';
         $.config.css += '</style><style>body{padding-top:2.5em}';
         $._.extend($.config.languages.tools, {
             maximize: ['Maximize', 'F11'],
