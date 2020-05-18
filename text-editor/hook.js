1f565b5352e69a3091bbeac56be2782eec01b4a3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-05-18 08:16:26 +0700

    Update Hook Plugin'

--- text-editor/hook.js
+++ text-editor/hook.js
@@ -1,9 +1,6 @@
-
-
-
 /*!
  * ==============================================================
- *  TEXT EDITOR HOOK 1.0.0
+ *  TEXT EDITOR HOOK 1.0.1
  * ==============================================================
  * Author: Taufik Nurrohman <https://github.com/taufik-nurrohman>
  * License: MIT
@@ -12,52 +9,112 @@
 
 (function(win, doc, name) {
 
-    var $ = win[name],
-        _ = $._,
-        _hooks = {};
+    var $$ = win[name],
+
+        call = 'call',
+        forEach = 'forEach',
+
+        $$$, prop;
+
+    function isFunction(x) {
+        return 'function' === typeof x;
+    }
 
     function isSet(x) {
         return 'undefined' !== typeof x;
     }
 
-    _.fire = function(name, lot) {
-        if (!isSet(_hooks[name])) {
-            return this;
-        }
-        for (var i = 0, j = _hooks[name].length; i < j; ++i) {
-            _hooks[name][i].apply(this, lot);
-        }
-        return this;
-    };
+    $$$ = function(source, state) {
 
-    _.hooks = _hooks;
+        var $ = this,
+            hooks = {},
+            nativeEvents = [
+                'blur',
+                'click',
+                'focus',
+                'input',
+                'keydown',
+                'keypress',
+                'keyup',
+                'select'
+            ],
+            pop = $.pop;
 
-    _.off = function(name, fn) {
-        if (!isSet(name)) {
-            return (_hooks = {}), this;
-        }
-        if (isSet(_hooks[name])) {
-            if (isSet(fn)) {
-                for (var i = 0, j = _hooks[name].length; i < j; ++i) {
-                    if (fn === _hooks[name][i]) {
-                        _hooks[name].splice(i, 1);
-                    }
-                }
-            } else {
-                delete _hooks[name];
+        // Is the same as `parent::__construct()` in PHP
+        $$[call]($, source, state);
+
+        $.fire = function(name, lot) {
+            if (!isSet(hooks[name])) {
+                return $;
             }
+            for (var i = 0, j = hooks[name].length; i < j; ++i) {
+                hooks[name][i].apply($, lot);
+            }
+            return $;
+        };
+
+        $.hooks = hooks;
+
+        $.off = function(name, fn) {
+            if (!isSet(name)) {
+                return (hooks = {}), $;
+            }
+            if (isSet(hooks[name])) {
+                if (isSet(fn)) {
+                    for (var i = 0, j = hooks[name].length; i < j; ++i) {
+                        if (fn === hooks[name][i]) {
+                            hooks[name].splice(i, 1);
+                        }
+                    }
+                    // Clean-up empty hook(s)
+                    if (0 === j) {
+                        delete hooks[name];
+                    }
+                } else {
+                    delete hooks[name];
+                }
+            }
+            return $;
+        };
+
+        $.on = function(name, fn) {
+            if (!isSet(hooks[name])) {
+                hooks[name] = [];
+            }
+            if (isSet(fn)) {
+                hooks[name].push(fn);
+            }
+            return $;
+        };
+
+        function doApplyHookToNativeEvents(e) {
+            $.fire(e.type, [e]);
         }
-        return this;
+
+        // Apply hook to all event(s)
+        nativeEvents[forEach](function(n) {
+            source.addEventListener(n, doApplyHookToNativeEvents, false);
+        });
+
+        $.pop = function() {
+            // Remove hook from all event(s)
+            nativeEvents[forEach](function(n) {
+                source.removeEventListener(n, doApplyHookToNativeEvents);
+            });
+            return isFunction(pop) ? pop[call]($) : $;
+        }
+
     };
 
-    _.on = function(name, fn) {
-        if (!isSet(_hooks[name])) {
-            _hooks[name] = [];
-        }
-        if (isSet(fn)) {
-            _hooks[name].push(fn);
-        }
-        return this;
-    };
+    // Clone all static property from the old constructor
+    for (prop in $$) {
+        $$$[prop] = $$[prop];
+    }
+
+    // Clone prototype(s)
+    $$$.prototype = $$$._ = $$._;
+
+    // Override
+    win[name] = $$$;
 
 })(this, this.document, 'TE');
